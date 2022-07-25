91c3076eb7546a77cd57accf031dd25856c70605
alejandro2Lopez <alejandro@DESKTOP-RHK3T75.localdomain>
2022-07-24 21:31:52 -0600

    Confirmar pedido listo

--- src/pages/AddToCars/AddToCar.tsx
+++ src/pages/AddToCars/AddToCar.tsx
@@ -1,80 +1,89 @@
 import React, { useEffect, useState } from "react";
-import { IonPage,IonMenuButton, IonHeader, IonItem,IonButtons,IonImg, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonContent, IonButton, IonTitle, IonModal, IonCol } from '@ionic/react';
+import { IonPage, IonMenuButton, IonHeader, IonItem, IonButtons, IonImg, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonContent, IonButton, IonTitle, IonModal, IonCol } from '@ionic/react';
 import UseApi from "../../components/UseApi";
 import './AddToCar.css'
 const AddToCar: React.FC = () => {
-    const { data, delet} = UseApi(`${process.env.REACT_APP_API_URL}/api/order_details`);
-    const [costt, setCost]=useState(0);
-   
-   useEffect(()=>{
-    var a =0;
-    data?.map((order_detail: any) => { 
-    a = (a +parseInt(order_detail?.cost));
+  const { data, delet, refetch } = UseApi(`${process.env.REACT_APP_API_URL}/api/order_details`);
+  const [costt, setCost] = useState(0);
+
+  useEffect(() => {
+    var a = 0;
+    data?.map((order_detail: any) => {
+      a = (a + parseInt(order_detail?.cost));
       setCost(a)
     })
 
-   })
-    const destroy =(id=0)=>{
-     
-      try {
-        delet(id)
+  })
+  const destroy = (id = 0) => {
+
+    try {
+      delet(id)
     } catch (err) {
-        console.log(err)
+      console.log(err)
     }
-}
-    if (!data) {
-        return <h1>Cargando...</h1>
+  }
+  const confirmar = () => {
+    try {
+      refetch({ client_id: 1, isConfirm: 1, cost:costt});
+
+    } catch (err) {
+      console.log(err)
     }
- 
-    return (
-        <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-            <IonTitle>Car</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+  }
+
+  if (!data) {
+    return <h1>Cargando...</h1>
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Car</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Car</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
-            {data?.map((order_detail: any) => {      
-               return (
-                    <IonCard key={order_detail.id}>
-                        <IonCardHeader>
-                            <IonCardTitle> Dish: {order_detail?.name}</IonCardTitle>
-                            <IonCardTitle> Cost: {order_detail?.cost}</IonCardTitle>
-                        
-                        </IonCardHeader>
-                        <IonCardContent>{data?.delivery}</IonCardContent>
-                       
-                    <form onSubmit={() => destroy(order_detail?.id)   }>
-                     <IonButton type="submit" class="center" >destroy</IonButton>
-                     </form>
-                    </IonCard>
-                )
-            })}
-             <IonItem>
-       
-                 <h2>Precio: {costt}</h2>
-                 <form onSubmit={() => destroy(1)   }>
-                     <IonButton type="submit" class="buttonBuy" >Confimar</IonButton>
-                     </form>
-                 </IonItem>
+          {data?.map((order_detail: any) => {
+            return (
+              <IonCard key={order_detail.id}>
+                <IonCardHeader>
+                  <IonCardTitle> Dish: {order_detail?.name}</IonCardTitle>
+                  <IonCardTitle> Cost: {order_detail?.cost}</IonCardTitle>
+
+                </IonCardHeader>
+                <IonCardContent>{data?.delivery}</IonCardContent>
+
+                <form onSubmit={() => destroy(order_detail?.id)}>
+                  <IonButton type="submit" class="center" >destroy</IonButton>
+                </form>
+              </IonCard>
+            )
+          })}
+          <IonItem>
+
+            <h2>Precio: {costt}</h2>
+            <form onSubmit={() => confirmar()}>
+              <IonButton type="submit" class="buttonBuy" >Confimar</IonButton>
+            </form>
+          </IonItem>
         </IonContent>
-   
+
       </IonContent>
 
 
-      </IonPage>
+    </IonPage>
 
-   
-    )
+
+  )
 };
 
 export default AddToCar;
